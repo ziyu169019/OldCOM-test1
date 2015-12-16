f5b203d947235be3441694f5e3caf32a666e6357
beansYan <913244892@qq.com>
2015-12-16 17:36:18 +0800

    Initial commit

---
+++ app/src/main/java/com/example/yan/compro/SplashScreen.java
@@ -0,0 +1,53 @@
+package com.example.yan.compro;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.widget.TextView;
+import android.graphics.PixelFormat;
+
+
+
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.content_splash_screen);
+
+        PackageManager pm = getPackageManager();
+        try {
+            PackageInfo pi = pm.getPackageInfo("com.example.yan.compro", 0);
+            TextView versionNumber = (TextView) findViewById(R.id.versionNumber);
+            versionNumber.setText("Version " + pi.versionName);
+        } catch (NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                /* Create an Intent that will start the Main WordPress Activity. */
+                Intent mainIntent = new Intent(SplashScreen.this, MainTabActivity.class);
+                SplashScreen.this.startActivity(mainIntent);
+                SplashScreen.this.finish();
+            }
+        }, 2900); //2900 for release
+    }
+
+}
